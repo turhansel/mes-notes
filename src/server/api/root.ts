44efdf518c219171497c889a9b265e3c9064bb88
Turhan <turhansel@gmail.com>
2023-02-25 21:03:20 +0300

    feat: create `topicRouter`

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { exampleRouter } from "@/server/api/routers/example";
+import { topicRouter } from "./routers/topic";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "@/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topic: topicRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/topic.ts
@@ -0,0 +1,25 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { z } from "zod";
+
+export const topicRouter = createTRPCRouter({
+  getAll: protectedProcedure
+    .input(z.object({ authorId: z.string() }))
+    .query(({ ctx }) => {
+      return ctx.prisma.topic.findMany({
+        where: {
+          authorId: ctx.session.user.id,
+        },
+      });
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.topic.create({
+        data: {
+          title: input.title,
+          authorId: ctx.session.user.id,
+        },
+      });
+    }),
+});
