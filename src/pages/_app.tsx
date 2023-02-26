20f15a7013cffe1f413952df88fdaba054ead561
Turhan <turhansel@gmail.com>
2023-02-26 22:00:41 +0300

    feat: add `react-wrap-balancer` to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import "@/styles/globals.css";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Provider as RWBProvider } from "react-wrap-balancer";
 import { Inter } from "@next/font/google";
-import "@/styles/globals.css";
 import { api } from "@/lib/utils/api";
 
 const inter = Inter({
@@ -16,9 +17,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <RWBProvider>
         <main className={`${inter.variable} font-sans`}>
           <Component {...pageProps} />
         </main>
+      </RWBProvider>
     </SessionProvider>
   );
 };
