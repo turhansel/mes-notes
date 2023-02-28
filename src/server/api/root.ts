f843e037b0bf97535ebd3b4416dc6484091decaf
Turhan <turhansel@gmail.com>
2023-02-28 21:51:25 +0300

    feat: add `noteRouter` to `appRouter`

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { exampleRouter } from "@/server/api/routers/example";
 import { topicRouter } from "./routers/topic";
+import { noteRouter } from "./routers/note";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { topicRouter } from "./routers/topic";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
