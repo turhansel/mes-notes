d2572ad38ba6ac1bdc5132f7819de7d0a7e746a3
Turhan <turhansel@gmail.com>
2023-02-28 21:49:48 +0300

    feat: update `Header` component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import { FADE_IN_ANIMATION_SETTINGS } from "@/lib/constants";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import UserDropdown from "./UserDropdown";
+import { useSignInModal } from "../modals/SignInModal";
 
 const Header: React.FC = () => {
   const { data: session, status } = useSession();
+  const { setShowSignInModal, SignInModal } = useSignInModal();
 
   const isLoggedIn = !session && status !== "loading";
   return (
     <>
+      <SignInModal />
       <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-      <div
-        className={`fixed top-0 z-30 w-full border-b border-gray-200 bg-white/50 backdrop-blur-xl transition-all`}
-      >
+      <div className="fixed top-0 z-30 w-full border-b border-gray-200 bg-white/50 backdrop-blur-xl transition-all">
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
           <Link href="/" className="font-display flex items-center text-2xl">
             {session?.user?.name
@@ -26,7 +27,7 @@ const Header: React.FC = () => {
               {isLoggedIn ? (
                 <motion.button
                   className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:border-primary hover:bg-primary hover:text-white"
-                  // open
+                  onClick={() => setShowSignInModal(true)}
                   {...FADE_IN_ANIMATION_SETTINGS}
                 >
                   Sign In
