98192e08ecc837ddc877d7c275d9f13e84fb9eb5
Turhan <turhansel@gmail.com>
2023-03-01 21:42:16 +0300

    feat: create `LoadingSpinner` component

---
+++ src/components/shared/icons/LoadingSpinner/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./LoadingSpinner.module.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className={styles.spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
