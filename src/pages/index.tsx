6b2cb735c9d8eeb8b9953e4ffa81d22b93f94848
Turhan <turhansel@gmail.com>
2023-03-04 17:00:12 +0300

    feat: create `HomePage`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,57 @@
 import { type NextPage } from "next";
 import Layout from "@/components/layout";
+import { motion } from "framer-motion";
+import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
+import Balancer from "react-wrap-balancer";
+import { Github } from "lucide-react";
 
 const Home: NextPage = () => {
-  return <Layout>homepage</Layout>;
+  return (
+    <Layout>
+      <motion.div
+        className="max-w-xl px-5 xl:px-0"
+        initial="hidden"
+        whileInView="show"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+      >
+        <motion.h1
+          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>Mes Notes as your daily routine</Balancer>
+        </motion.h1>
+        <motion.p
+          className="mt-6 text-center text-gray-500 md:text-xl"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>A note app for your daily routines and plans.</Balancer>
+        </motion.p>
+        <motion.div
+          className="mx-auto mt-6 flex items-center justify-center space-x-5"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <a
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
+            href="https://github.com/turhansel/mes-notes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github />
+            <p>Star on GitHub</p>
+          </a>
+        </motion.div>
+      </motion.div>
+    </Layout>
+  );
 };
 
 export default Home;
