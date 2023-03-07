8b23ffb9dcdefd8b1a19ff8d5835893e6482dcf2
Turhan <turhansel@gmail.com>
2023-03-07 19:29:09 +0300

    feat: create `[topicId]` dynamic route and manage topics with dynamic route

---
+++ src/pages/notes/[topicId].tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import Content from "@/containers/Content/Content";
+import { type NextPage } from "next";
+import React from "react";
+
+const TopicDetail: NextPage = () => {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
+};
+
+export default TopicDetail;
