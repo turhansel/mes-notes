eb7d7603769e010c862edcad164e67eecb036d01
Turhan <turhansel@gmail.com>
2023-03-07 22:28:21 +0300

    feat: redirect user to `/` when trying to access `notes/:path*` pages

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -47,6 +47,9 @@ export const authOptions: NextAuthOptions = {
     },
   },
   adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/",
+  },
   providers: [
     GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
