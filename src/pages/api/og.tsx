261fad5f18002fad71d55631701dd081b5c44cbd
Turhan <turhansel@gmail.com>
2023-03-10 01:01:06 +0300

    feat: create `og` image

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,53 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default async function handler(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const title = searchParams.get("title") || "Mes Notes";
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+          backgroundImage:
+            "linear-gradient(to bottom right, #E0E7FF 25%, #ffffff 50%, #CFFAFE 75%)",
+        }}
+      >
+        <img
+          src={new URL("../../public/logo.png", import.meta.url).toString()}
+          alt="Mes Notes Logo"
+          tw="w-20 h-20 mb-4 opacity-95"
+        />
+        <h1
+          style={{
+            fontSize: "100px",
+            fontFamily: "--font-inter",
+            background:
+              "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
+            backgroundClip: "text",
+            color: "transparent",
+            lineHeight: "5rem",
+            letterSpacing: "-0.02em",
+          }}
+        >
+          {title}
+        </h1>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
+}
