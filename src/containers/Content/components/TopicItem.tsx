9b0766dd7c4b962c043e9df3ba368862fbfb54ba
Turhan <turhansel@gmail.com>
2023-03-17 22:48:55 +0300

    feat: update `TopicItem` styling

--- src/containers/Content/components/TopicItem.tsx
+++ src/containers/Content/components/TopicItem.tsx
@@ -1,4 +1,6 @@
+import { api } from "@/lib/utils/api";
 import { type Topic } from "@prisma/client";
+import { Trash2 } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
@@ -8,10 +10,32 @@ const TopicItem: React.FC<{ topic: Topic }> = ({ topic }) => {
   const topicId = router.query.topicId as string;
 
   const isSelected = topicId === topic.id;
+
+  const handleRemove = (id: string) => {
+    console.log("id", id);
+  };
+
   return (
-    <li key={topic.id} className={isSelected ? "bg-primary" : ""}>
-      <Link href={`/notes/${topic.id}`}>{topic.title}</Link>
-    </li>
+    <Link href={`/notes/${topic.id}`}>
+      <li
+        key={topic.id}
+        className={`flex items-center justify-between rounded-md p-4 ${
+          isSelected ? "bg-orange-300" : undefined
+        }`}
+      >
+        <span>{topic.title}</span>
+        <button
+          type="button"
+          className="text-red-500"
+          onClick={(e) => {
+            e.preventDefault();
+            handleRemove(topic.id);
+          }}
+        >
+          <Trash2 className="h-5 w-5 text-red-500" />
+        </button>
+      </li>
+    </Link>
   );
 };
 
